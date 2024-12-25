de78cc947650d1ac0c7b9dd15bbfa8f1b402d810
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2024-12-25 10:12:15 +0530

    Time: 3 ms (20.65%), Space: 9.2 MB (31.5%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int n = s.size();
-        if (n == 0 || s[0] == '0') {
+        int n=s.size();
+        vector<int>dp(n+1,0);
+        if (s[0]=='0'){
             return 0;
         }
-
-        vector<int> dp(n, 0);
-        dp[0] = 1;
-
-        for (int i = 1; i < n; i++) {
-            if (s[i] != '0') {
-                dp[i] = dp[i - 1];
-            }
-
-            int twoDigit = stoi(s.substr(i - 1, 2));
-            if (twoDigit >= 10 && twoDigit <= 26) {
-                if (i > 1) {
-                    dp[i] += dp[i - 2];
-                } else {
-                    dp[i] += 1;
+        dp[0]=1;
+        dp[1]=1;
+        for (int i=2;i<=n;i++){
+            int two=stoi(s.substr(i-2,2));
+            if (s[i-1]=='0'){
+                if (two>=10 && two<=26){
+                    dp[i]+=dp[i-2];
                 }
+                
+            }
+            else{
+                if (two>=10 && two<=26){
+                    dp[i]+=dp[i-1]+dp[i-2];
+                }
+                else{
+                    dp[i]+=dp[i-1];
+
+                }
+                
+
+                
+
             }
         }
+        return dp[n];
 
-        return dp[n - 1];
+
+
+
+        
+
+        
+
     }
 };
