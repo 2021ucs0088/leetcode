9fdb4692ea0c04868fdfbde845fc08e60a1eaed7
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-01-08 09:49:15 +0530

    Time: 442 ms (5.09%), Space: 34.2 MB (54.82%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,11 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n=len(s)
+        dp = [[] for _ in range(n + 1)]
+        dp[0]=[[]]
+        for i in range(1,n+1):
+            for j in range(i):
+                if s[j:i]==s[j:i][::-1]:
+                    for ele in dp[j]:
+                        dp[i].append(ele+[s[j:i]])
+        return dp[n]
