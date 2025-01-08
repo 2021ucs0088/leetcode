83e85bf5e272fc119485481b02f0edf843348c98
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-01-09 00:41:45 +0530

    Time: 3 ms (65.38%), Space: 10.2 MB (57.44%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>result;
-        vector<string>board(n,string(n,'.'));
-        func(result,board,n,0);
-        return  result;
-
-
-        
+        vector<string> board(n, string(n, '.'));
+        vector<vector<string>> result;
+        func(board, result, n, 0);
+        return result;
     }
-    bool is_valid(int row,int col,vector<string>& board,int n){
-        for (int i=0;i<n;i++){
-            if (board[i][col]=='Q'){
-                return false;
-            }
-        }
-        for (int i=0;i<n;i++){
-            if (board[row][i]=='Q'){
-                return false;
-            }
-        }
-        for (int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
-            if (board[i][j]=='Q'){
-                return false;
-            }
 
+    bool isvalid(int row, int col, vector<string>& board) {
+        // Check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return false;
         }
-        for (int i=row-1,j=col+1;i>=0&& j<=n-1;i--,j++){
-            if (board[i][j]=='Q'){
-                return false;
-            }
 
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
         }
+
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.size(); i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+
         return true;
     }
-    void func(vector<vector<string>>&result,vector<string>&board,int n,int row){
-        if (row==n){
+
+    void func(vector<string>& board, vector<vector<string>>& result, int n, int row) {
+        if (row == n) {
             result.push_back(board);
+            return;
         }
-        for (int i=0;i<n;i++){
-            if (is_valid(row,i, board,n)){
-                board[row][i]='Q';
-                func(result,board,n,row+1);
-                board[row][i]='.';
+        for (int col = 0; col < n; col++) {
+            if (isvalid(row, col, board)) {
+                board[row][col] = 'Q';
+                func(board, result, n, row + 1);
+                board[row][col] = '.';
             }
         }
     }
