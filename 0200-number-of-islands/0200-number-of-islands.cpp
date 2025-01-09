71d8c4b56f6c2cdbfe607ecbbd33eedfe4d7935e
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-01-09 14:32:49 +0530

    Time: 77 ms (10.34%), Space: 32.3 MB (8.05%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        int count = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    q.push({i, j});
+                }
+                while (!q.empty()) {
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+                    for (auto d : dir) {
+                        int newx = x + d[0];
+                        int newy = y + d[1];
+                        if (newx >= 0 && newy >= 0 && newx < r && newy < c &&
+                            grid[newx][newy] == '1') {
+                            q.push({newx, newy});
+                            grid[newx][newy] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
