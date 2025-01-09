104cbf47f19f0f048c1b0cde56fe199ba7df58ea
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-01-09 22:27:17 +0530

    Time: 3 ms (56.22%), Space: 27.5 MB (64.85%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int r=grid.size();
-        int c=grid[0].size();
-        if(grid.empty()){
+        if (grid.empty()) {
             return 0;
         }
-        queue<pair<int,int>>q;
-        vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
-        vector<int>area;
-        int count=0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(grid[i][j]==1){
-                    q.push({i,j});
-                    grid[i][j]=0;
-                    count=1;
+        
+        int r = grid.size();
+        int c = grid[0].size();
+        int maxArea = 0; 
+        vector<int> area; 
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 1) {
+                    int count = 0; 
+                    func(i, j, grid, count);
+                    area.push_back(count); 
                 }
-                while(!q.empty()){
-                    int x=q.front().first;
-                    int y=q.front().second;
-                    q.pop();
-                    for(auto d:dir){
-                        int newx=x+d[0];
-                        int newy=y+d[1];
-                        if(newx>=0 && newy>=0 && newx<r && newy<c && grid[newx][newy] !=0){
-                            q.push({newx,newy});
-                            count++;
-                            
-                            grid[newx][newy]=0;
-                        }
-                        
-                    }
-                    
-
-                }
-                area.push_back(count);
             }
         }
-        return *max_element(area.begin(),area.end());
+        
+        if (area.empty()) {
+            return 0;
+        }
+        
+        return *max_element(area.begin(), area.end());
+    }
+    
+    void func(int i, int j, vector<vector<int>>& grid, int &count) {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0) {
+            return;
+        }
+        
+        grid[i][j] = 0; 
+        count++; 
+        func(i + 1, j, grid, count);
+        func(i - 1, j, grid, count);
+        func(i, j + 1, grid, count);
+        func(i, j - 1, grid, count);
     }
 };
