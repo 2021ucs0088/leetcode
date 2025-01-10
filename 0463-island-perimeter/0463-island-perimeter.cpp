afcad5f959afe2168a23331e19418c8f799ffc0c
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-01-10 10:28:08 +0530

    Time: 137 ms (5.01%), Space: 129.8 MB (5.05%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int r=grid.size();
-        int c=grid[0].size();
-        int result=0;
-        vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
-        for (int i=0;i<r;i++){
-            for (int j=0;j<c;j++){
-                if (grid[i][j]==1){
-                    result+=4;
-                    for (auto d:dir){
-                        int newr=i+d[0];
-                        int newc=j+d[1];
-                        if (newr>=0 && newc>=0 && newr<r && newc<c && grid[newr][newc] ==1 ){
-                            
-                            result-=1;
+        int r = grid.size();
+        int c = grid[0].size();
+        int ans = 0;
+        vector<vector<int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 1) {
+                    ans += 4;
+                    for (auto d : dir) {
+                        int newx = i + d[0];
+                        int newy = j + d[1];
+                        if(newx>=0 && newy>=0 && newx<r && newy<c && grid[newx][newy]==1){
+                            ans-=1;
 
                         }
-                        
                     }
                 }
             }
         }
-        return result;
+        return ans;
     }
 };
