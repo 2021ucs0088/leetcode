387a47d6d6cad6dd76aae98acd62bd511eac024f
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-01-24 12:14:36 +0530

    Time: 0 ms (100%), Space: 26.6 MB (59.54%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -3,40 +3,24 @@ public:
     int countServers(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
-        int total = 0;
+        vector<int> v1(r);
+        vector<int> v2(c);
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 if (grid[i][j] == 1) {
-                    total++;
+                    v1[i]++;
+                    v2[j]++;
                 }
             }
         }
-        int count = 0;
+        int ans = 0;
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 1) {
-                    if (func(grid, i, j)) {
-                        count++;
-                    }
+                if ( grid[i][j]==1 && (v1[i] >1  || v2[j] > 1)) {
+                    ans++;
                 }
             }
         }
-        return total - count;
-    }
-    bool func(vector<vector<int>>& grid, int row, int col) {
-        int r = grid.size();
-        int c = grid[0].size();
-
-        for (int i = 0; i < r; i++) {
-            if (grid[i][col] != 0 && i != row) {
-                return false;
-            }
-        }
-        for (int i = 0; i < c; i++) {
-            if (grid[row][i] != 0 && i != col) {
-                return false;
-            }
-        }
-        return true;
+        return ans;
     }
 };
