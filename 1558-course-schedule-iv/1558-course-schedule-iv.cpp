a48423029f646c89d9dab471c5ac0273302f9402
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-01-27 11:57:13 +0530

    Time: 191 ms (18.79%), Space: 73.8 MB (37.2%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.cpp
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        map<int,vector<int>>mp;
-        for(auto ele:prerequisites){
+    vector<bool> checkIfPrerequisite(int numCourses,
+                                     vector<vector<int>>& prerequisites,
+                                     vector<vector<int>>& queries) {
+        map<int, vector<int>> mp;
+        
+        for (auto ele : prerequisites) {
             mp[ele[0]].push_back(ele[1]);
         }
-        vector<bool>result(queries.size());
-        for(int i=0;i<queries.size();i++){
-            result[i]=func(queries[i][0],queries[i][1],mp);
-            
-
+        vector<bool> result(queries.size());
+        for (int i = 0; i < queries.size(); i++) {
+            vector<bool> vis(numCourses);
+            result[i] = func(queries[i][0], queries[i][1], mp, vis);
         }
         return result;
     }
-    bool func(int i,int j,map<int,vector<int>>&mp){
-        if (i==j){
+    bool func(int i, int j, map<int, vector<int>>& mp, vector<bool>& vis) {
+        if (i == j) {
             return true;
         }
-        for(auto ele:mp[i]){
-            if(ele==j){
-                return true;
-            }
-            else{
-                func(ele,j,mp);
+        vis[i] = true;
+        for (auto ele : mp[i]) {
+            if (vis[ele] == false) {
+                if(func(ele, j, mp, vis)){
+                    return true;
+                }
             }
         }
         return false;
-
     }
 };
