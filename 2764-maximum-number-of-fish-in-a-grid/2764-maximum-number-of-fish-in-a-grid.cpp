0733b17880746c115e67140676c2f7f9d7513ada
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-01-28 13:26:41 +0530

    Time: 0 ms (100%), Space: 94.1 MB (85.64%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     int findMaxFish(vector<vector<int>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
-        vector<int> result;
-        
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] != 0) {
-                    int count = 0;
-                    func(i, j, grid, count);
-                    result.push_back(count);
+        int r=grid.size();
+        int c=grid[0].size();
+        int maxval=INT_MIN;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]!=0){
+                    int count=0;
+                    func(i,j,grid,count);
+                    maxval=max(maxval,count);
                 }
             }
         }
-        return result.empty() ? 0 : *max_element(result.begin(), result.end());
+        if (maxval==INT_MIN){
+            return 0;
+        }
+        return maxval;
+
+    }
+    void func(int i,int j,vector<vector<int>>& grid ,int &count){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==0){
+            return ;
+
+        }
+        count+=grid[i][j];
+        grid[i][j]=0;
+        func(i+1,j,grid,count);
+        func(i-1,j,grid,count);
+        func(i,j-1,grid,count);
+        func(i,j+1,grid,count);
     }
 
-    void func(int i, int j, vector<vector<int>>& grid, int &count) {
-        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0) {
-            return;
-        }
-        count += grid[i][j];
-        grid[i][j] = 0;
-        func(i + 1, j, grid, count);
-        func(i - 1, j, grid, count);
-        func(i, j - 1, grid, count);
-        func(i, j + 1, grid, count);
-    }
 };
