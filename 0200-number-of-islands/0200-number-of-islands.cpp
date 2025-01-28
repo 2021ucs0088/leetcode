1ed964e9f30355e19771544a44c4974efa5cbcd1
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-01-28 13:41:33 +0530

    Time: 81 ms (9.71%), Space: 31.3 MB (8.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,26 +3,31 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
+        vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
+        queue<pair<int,int>>q;
         int count=0;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(grid[i][j]=='1'){
+                    q.push({i,j});
                     count++;
-                    func(i,j,grid);
+                    grid[i][j]='0';
+                }
+                while(!q.empty()){
+                    int x=q.front().first;
+                    int y=q.front().second;
+                    q.pop();
+                    for(auto d:dir){
+                        int newx=x+d[0];
+                        int newy=y+d[1];
+                        if(newx>=0 && newy>=0 && newx<r && newy<c && grid[newx][newy] =='1'){
+                            q.push({newx,newy});
+                            grid[newx][newy]='0';
+                        }
+                    }
                 }
             }
         }
         return count;
-
-    }
-    void func(int i ,int j,vector<vector<char>>& grid){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0'){
-            return ;
-        }
-        grid[i][j]='0';
-        func(i+1,j,grid);
-        func(i-1,j,grid);
-        func(i,j+1,grid);
-        func(i,j-1,grid);
     }
 };
