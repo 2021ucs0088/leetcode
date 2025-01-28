6a8078d8f543deac6019ea6b18ce5c247f29727b
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-01-28 13:36:50 +0530

    Time: 21 ms (91.78%), Space: 16.3 MB (67.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -15,12 +15,11 @@ public:
         return count;
 
     }
-    void func(int i,int j,vector<vector<char>>& grid){
-        if(i>=grid.size() || j>=grid[0].size() || i<0 || j<0 || grid[i][j]=='0'){
+    void func(int i ,int j,vector<vector<char>>& grid){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0'){
             return ;
         }
         grid[i][j]='0';
-        
         func(i+1,j,grid);
         func(i-1,j,grid);
         func(i,j+1,grid);
