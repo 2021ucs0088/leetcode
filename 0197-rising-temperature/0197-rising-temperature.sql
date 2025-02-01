217daf0a9557e4ab743d1d28df2ba4d16dea76e9
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-02-02 02:00:34 +0530

    Time: 746 ms (56.28%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,7 @@
 select w1.id
 from Weather w1
 inner join Weather w2
-where DATEDIFF(w1.recordDate,w2.recordDate) =1 and w1.temperature>w2.temperature   
+where Datediff(w1.recordDate ,w2.recordDate)=1
+and w1.temperature >w2.temperature 
+
 
