b8d5f5e0f9a503300a7aa922a0547f211575ffe0
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-02-02 01:40:59 +0530

    Time: 3892 ms (5%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,2 +1,5 @@
-select Product.product_name ,Sales.year,Sales.price 
-from Sales left join Product on Product.product_id=Sales.product_id
+select p.product_name ,s.year,s.price
+from Sales s
+left join
+Product p
+on s.product_id=p.product_id
