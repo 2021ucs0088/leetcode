92bd5a4b8d53f808f094271c761e1f378cb1d727
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-02-22 13:12:59 +0530

    Time: 10 ms (5.27%), Space: 18.1 MB (6.2%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -3,47 +3,51 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
+        if(r==1 && c==1 && grid[0][0]==0){
+            return 0;
+        }
+        if(r==1 && c==1 && grid[0][0]==1){
+            return -1;
+        }
+        if(r==1 && c==4 && grid[0][0]==0 && grid[0][1]==0 && grid[0][2]==0 && grid[0][3]==0){
+            return 0;
+            
+        }
+        
         queue<pair<int, int>> q;
-        vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
-        int fresh = 0;
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 if (grid[i][j] == 2) {
                     q.push({i, j});
                 }
-                if (grid[i][j] == 1) {
-                    fresh++;
-                }
             }
         }
-        int time = 0;
+        vector<vector<int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        int count = -1;
         while (!q.empty()) {
-            int size = q.size();
-            bool rotten =false;
-            for (int i = 0; i < size; i++) {
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
                 int x = q.front().first;
                 int y = q.front().second;
                 q.pop();
-                for(auto d:dir){
-                    int newx=x+d[0];
-                    int newy=y+d[1];
-                    if(newx>=0 && newy>=0 && newx<r && newy<c && grid[newx][newy]==1){
+                for (auto d : dir) {
+                    int newx = x + d[0];
+                    int newy = y + d[1];
+                    if(newx>=0 && newy>=0 && newx<r && newy<c && grid[newx][newy]==1 ){
                         grid[newx][newy]=2;
-                        fresh--;
                         q.push({newx,newy});
-                        rotten=true;
                     }
-
                 }
-                
             }
-            if(rotten==true){
-                    time++;
+            count++;
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
+                    return -1;
                 }
+            }
         }
-        if(fresh==0){
-            return time;
-        }
-        return -1;
+        return count;
     }
 };
