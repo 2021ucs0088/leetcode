d8ec024d55248e726367e7a293745c26c0deb1b4
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-02-23 12:44:33 +0530

    Time: 1121 ms (5.02%), Space: 346.1 MB (5.01%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
-        int r = isWater.size();
-        int c = isWater[0].size();
-        queue<pair<int, int>> q;
-        vector<vector<int>> result(r, vector<int>(c, INT_MAX - 1));
-        
-        // Initialize water cells
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (isWater[i][j] == 1) {
-                    q.push({i, j});
-                    result[i][j] = 0;
+        int r=isWater.size();
+        int c=isWater[0].size();
+        vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
+        vector<vector<int>>dp(r,vector<int>(c,INT_MAX));
+        queue<pair<int,int>>q;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(isWater[i][j]==1){
+                    q.push({i,j});
+                    dp[i][j]=0;
                 }
             }
         }
-
-        // Directions for BFS
-        vector<vector<int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        
-        // BFS traversal
-        while (!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
             q.pop();
-
-            for (auto d : dir) {
-                int newx = x + d[0];
-                int newy = y + d[1];
-                if (newx >= 0 && newy >= 0 && newx < r && newy < c && result[newx][newy] == INT_MAX - 1) {
-                    result[newx][newy] = result[x][y] + 1;
-                    q.push({newx, newy});
+            for(auto d:dir){
+                int newx=x+d[0];
+                int newy=y+d[1];
+                if(newx>=0 && newy>=0 && newx<r && newy<c && isWater[newx][newy]==0){
+                    dp[newx][newy]=min(dp[newx][newy],dp[x][y]+1);
+                    isWater[newx][newy]=1;
+                    q.push({newx,newy});
                 }
             }
         }
+        return dp;
 
-        return result;
     }
 };
