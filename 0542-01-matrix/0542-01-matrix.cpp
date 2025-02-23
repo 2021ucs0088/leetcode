e77b8ee5e998bd042358ee3c0f555e795ba6f545
2021ucs0088 <164010630+2021ucs0088@users.noreply.github.com>
2025-02-23 12:30:06 +0530

    Time: 193 ms (5.12%), Space: 74.8 MB (5.19%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,19 +3,13 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int r=mat.size();
         int c=mat[0].size();
+        vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
+        vector<vector<int>>dp(r,vector<int>(c,INT_MAX));
         queue<pair<int,int>>q;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(mat[i][j]==1){
-                    q.push({i,j});
-                }
-            }
-        }
-        vector<vector<int>>dp(r,vector<int>(c,INT_MAX-1));
-        vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
                 if(mat[i][j]==0){
+                    q.push({i,j});
                     dp[i][j]=0;
                 }
             }
@@ -27,16 +21,20 @@ public:
             for(auto d:dir){
                 int newx=x+d[0];
                 int newy=y+d[1];
-                if(newx>=0 && newy>=0 && newx<r && newy<c){
-                    dp[x][y]=min(dp[x][y],dp[newx][newy]+1);
+                if(newx>=0 && newy>=0 && newx<r && newy<c && mat[newx][newy]==1){
+                    dp[newx][newy]=min(dp[newx][newy],dp[x][y]+1);
+                    q.push({newx,newy});
+                    mat[newx][newy]=0;
                     
+                    
+
+
+
                 }
+                
             }
-
-
+            
         }
         return dp;
-        
-
     }
 };
